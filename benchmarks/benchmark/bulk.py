88dc74846f2df953051a51db3ceaf379a95edc93
Georg Richter <georg@mariadb.com>
2020-01-04 17:07:52 +0100

    Added benchmark for bulk (insert/update/delete)

---
+++ benchmarks/benchmark/bulk.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3 -O
+# -*- coding: utf-8 -*-
+
+import pyperf
+
+
+def bulk(loops, conn):
+    cursor = conn.cursor()
+    cursor.execute("DROP TEMPORARY TABLE IF EXISTS test_update_bulk")
+    cursor.execute("CREATE TEMPORARY TABLE test_update_bulk (a int primary key, b int)")
+    t0 = pyperf.perf_counter()
+    vals = [(i,) for i in range(10000)]
+    range_it = range(loops)
+    for value in range_it:
+        cursor.executemany("INSERT INTO test_update_bulk VALUES (%s, NULL)", vals);
+        conn.commit()
+        cursor.executemany("UPDATE test_update_bulk SET b=2 WHERE a=%s", vals);
+        conn.commit()
+        cursor.executemany("DELETE FROM test_update_bulk WHERE a=%s", vals);
+    del cursor
+    return pyperf.perf_counter() - t0
--- benchmarks/internal_bench.py
+++ benchmarks/internal_bench.py
@@ -9,6 +9,7 @@ from benchmarks.benchmark.select_param import select_param
 from benchmarks.benchmark.select_10_cols_from_seq_1_to_10000 import \
     select_10_cols_from_seq_1_to_10000
 from benchmarks.benchmark.select_1_mysql_user import select_1_mysql_user
+from benchmarks.benchmark.bulk import bulk
 
 
 def run_test(tests, conn):
@@ -23,6 +24,7 @@ def test_suite():
         {'label': 'select 1', 'method': select_1},
         {'label': 'select param', 'method': select_param},
         {'label': 'select 1 mysql user', 'method': select_1_mysql_user},
+        {'label': 'bulk: insert/update/delete', 'method': bulk},
         {'label': 'Select <10 cols of 100 chars> from_seq_1_to_100000', 'method':
             select_10_cols_from_seq_1_to_10000}
     ]
