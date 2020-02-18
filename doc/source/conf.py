4ac55f3b29c7cb70c2ef1b75f6c715369d0f210c
Georg Richter <georg@mariadb.com>
2020-02-18 15:01:52 +0100

    Remove sphinx markdown builder

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -31,7 +31,7 @@ release = '0.9.54 beta'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx_markdown_builder', 'sphinx.ext.intersphinx',
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
