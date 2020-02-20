f152bffe41e79dadcc2407672b714499ba46bef4
Georg Richter <georg@mariadb.com>
2020-02-20 09:51:27 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ cfg = get_config(options)
 
 
 setup(name='mariadb',
-      version='0.9.54',
+      version='0.9.55',
       python_requires='>=3.6',
       classifiers = [
           'Development Status :: 4 - Beta',
