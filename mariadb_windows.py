7972976e91c812010d65b0ddec959f9e12b7d681
rusher <diego.dupin@gmail.com>
2024-02-05 19:32:07 +0100

    [misc] test

--- mariadb_windows.py
+++ mariadb_windows.py
@@ -64,9 +64,9 @@ def get_config(options):
 
     print("Found MariaDB Connector/C in '%s'" % mariadb_dir)
     cfg = MariaDBConfiguration()
-    cfg.includes = [".\\include", mariadb_dir + "\\include", mariadb_dir +
-                    "\\include\\mysql", mariadb_dir + "\\include\\mariadb"]
-    cfg.lib_dirs = [mariadb_dir + "\\lib", mariadb_dir + "\\lib\\mariadb"]
+    cfg.includes = ["./include", mariadb_dir + "/include", mariadb_dir +
+                    "/include/mysql", mariadb_dir + "/include/mariadb"]
+    cfg.lib_dirs = [mariadb_dir + "/lib", mariadb_dir + "/lib/mariadb"]
     cfg.libs = ["ws2_32", "advapi32", "kernel32", "shlwapi", "crypt32",
                 "secur32", "bcrypt"]
     if static.lower() == "on" or static.lower() == "default":
