84147b935125bc677c7566090f374380144f26e7
Diego Dupin <diego.dupin@mariadb.com>
2025-11-05 22:52:26 +0100

    [misc] use SPDX license identifier

--- mariadb_shared/constants/SESSION_TRACK.py
+++ mariadb_shared/constants/SESSION_TRACK.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+# Copyright (c) 2020-2025 MariaDB Corporation Ab
+
 # Session tracking types for OK packet session state information
 # See: https://mariadb.com/kb/en/ok_packet/#session-tracking
 
--- mariadb_shared/constants/TPC_STATE.py
+++ mariadb_shared/constants/TPC_STATE.py
@@ -1,3 +1,6 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+# Copyright (c) 2020-2025 MariaDB Corporation Ab
+
 NONE = 0
 XID = 1
 PREPARE = 2
